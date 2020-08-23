1221995c882290e9de86049a22752bda3dae392d
julesfll <jfl4kb@virginia.edu>
2020-08-23 13:40:22 -0400

    Add bounding boxes to faces and login components

--- src/App.js
+++ src/App.js
@@ -3,9 +3,16 @@ import Navigation from './components/Navigation/Navigation';
 import Logo from './components/Logo/Logo';
 import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm';
 import Rank from './components/Rank/Rank';
+import FaceRecognition from './components/FaceRecognition/FaceRecognition';
+import Signin from './components/Signin/Signin';
+import Register from './components/Register/Register';
 import Particles from 'react-particles-js';
+import Clarifai from 'clarifai';
 import './App.css';
 
+const app = new Clarifai.App({
+  apiKey: 'f9df86dfece3466cb2cc27a89fb42e1b',
+});
 const particlesOptions = {
   // check documentation, it doesn't seem to work
   particles: {
@@ -24,29 +31,78 @@ class App extends Component {
     super();
     this.state = {
       input: '',
+      imageUrl: '',
+      boxes: [],
+      route: 'signin',
+      isSignedIn: false,
     };
   }
 
+  calculateFaceLocation = (data) => {
+    const clarifaiFaces = data.outputs[0].data.regions;
+    return clarifaiFaces.map((region) => {
+      const clarifaiFace = region.region_info.bounding_box;
+      return {
+        leftCol: clarifaiFace.left_col * 100 + '%',
+        topRow: clarifaiFace.top_row * 100 + '%',
+        rightCol: (1 - clarifaiFace.right_col) * 100 + '%',
+        bottomRow: (1 - clarifaiFace.bottom_row) * 100 + '%',
+      };
+    });
+  };
+
   onInputChange = (event) => {
-    console.log(event.target.value);
+    this.setState({ input: event.target.value });
   };
 
   onButtonSubmit = () => {
-    console.log('submitted');
+    this.setState({ imageUrl: this.state.input });
+    app.models
+      .predict(Clarifai.FACE_DETECT_MODEL, this.state.input)
+      .then((response) =>
+        this.setState({ boxes: this.calculateFaceLocation(response) })
+      )
+      .catch((err) => console.log(err));
   };
 
+  onRouteChange = (route) => {
+    if (route === 'signout') {
+      this.setState({ isSignedIn: false });
+    } else if (route === 'home') {
+      this.setState({ isSignedIn: true });
+    }
+    // this.setState({ isSignedIn: route === 'home });
+    this.setState({ route });
+  };
+  // == this.setState({route: route})
+
   render() {
+    const { isSignedIn, imageUrl, route, boxes } = this.state;
     return (
       <div className="App">
         <Particles className="particles" params={particlesOptions} />
-        <Navigation />
-        <Logo />
-        <Rank />
-        <ImageLinkForm
-          onInputChange={this.onInputChange}
-          onSubmit={this.onButtonSubmit}
+        <Navigation
+          onRouteChange={this.onRouteChange}
+          isSignedIn={isSignedIn}
         />
-        {/* <FaceRecognition /> */}
+        {route === 'home' ? (
+          <div>
+            <Logo />
+            <Rank />
+            <ImageLinkForm
+              onInputChange={this.onInputChange}
+              onButtonSubmit={this.onButtonSubmit}
+            />
+            <FaceRecognition
+              boxes={boxes}
+              imageUrl={imageUrl}
+            />
+          </div>
+        ) : route === 'signin' ? (
+          <Signin onRouteChange={this.onRouteChange} />
+        ) : (
+          <Register onRouteChange={this.onRouteChange} />
+        )}
       </div>
     );
   }
---
+++ src/components/FaceRecognition/FaceRecognition.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import './FaceRecognition.css';
+
+const FaceRecognition = ({ imageUrl, boxes }) => {
+  return (
+    <div className="center ma">
+      <div className="absolute mt2">
+        <img
+          id="inputimage"
+          alt=""
+          src={imageUrl}
+          width="500px"
+          height="auto"
+        />
+        {boxes.map((box, index) => (
+          <div
+            className="bounding-box"
+            key={index}
+            style={{
+              top: box.topRow,
+              right: box.rightCol,
+              bottom: box.bottomRow,
+              left: box.leftCol,
+            }}
+          ></div>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default FaceRecognition;
